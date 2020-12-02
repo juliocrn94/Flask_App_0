74368ff1f16c3a5042711f62965f27e217499d6a
julio <julio@iMac-de-julio.local>
2020-12-02 16:01:17 -0600

    added html template

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+to_dos = ['ToDo1','ToDo2','ToDo3']
 
 @app.route('/')
 def index():
@@ -14,8 +15,11 @@ def index():
 
 @app.route('/hello')
 def hello():
-
     user_ip = request.cookies.get('user_ip')
-    
-    return 'Hello World Platzi, tu IP es {}'.format(user_ip)
+    context = {
+        'user_ip':user_ip,
+        'to_dos':to_dos,
+    }
+
+    return render_template('hello.html', **context)
 
