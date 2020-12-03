e4893d159015fc5201f41ba39b31e2fc328061a3
julio <julio@iMac-de-julio.local>
2020-12-03 13:02:43 -0600

    added flash messages

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session,url_for,flash
+
 from flask_bootstrap import Bootstrap
+
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -8,6 +14,12 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 
 to_dos = ['Comprar Cafe','Enviar Tarea','Hacer Reporte']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario',validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -26,14 +38,27 @@ def index():
     
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET','POST'])
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip':user_ip,
         'to_dos':to_dos,
+        'login_form':login_form,
+        'username':username
     }
 
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username']=username
+
+        flash('Nombre de Usuario Registrado con Exito')
+
+        return redirect(url_for('index'))
+    
     return render_template('hello.html', **context)
 
 @app.route('/error500')
