400e048949524f5fe2037572e1a49c25f394a08d
julio <julio@iMac-de-julio.local>
2020-12-04 15:54:15 -0600

    adding login

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import unittest
 from flask import Flask, request, make_response, redirect, render_template, session,url_for,flash
 
 from flask_bootstrap import Bootstrap
-
-import unittest
+from flask_login import login_required
 
 from app import create_app
 from app.forms import LoginForm
+from app.firestore_service import get_users, get_todos
+
 
 app = create_app()
-
-to_dos = ['Comprar Cafe','Enviar Tarea','Hacer Reporte']
 
 
 @app.cli.command()
@@ -35,16 +35,17 @@ def index():
     return response
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
     username = session.get('username')
 
     context = {
         'user_ip':user_ip,
-        'to_dos':to_dos,
+        'to_dos':get_todos(username),
         'username':username
     }
-    
+
     return render_template('hello.html', **context)
 
 @app.route('/error500')
