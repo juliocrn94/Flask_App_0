ec2ebbcb749a4d20bc34b8253d4c08959f70c464
julio <julio@iMac-de-julio.local>
2020-12-04 16:48:34 -0600

    added signup

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import unittest
 from flask import Flask, request, make_response, redirect, render_template, session,url_for,flash
 
 from flask_bootstrap import Bootstrap
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from app import create_app
 from app.forms import LoginForm
@@ -38,7 +38,7 @@ def index():
 @login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
 
     context = {
         'user_ip':user_ip,
