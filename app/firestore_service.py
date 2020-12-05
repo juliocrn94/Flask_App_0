9da8b32f6a8ed719eff9037add437d62e79be89d
julio <julio@iMac-de-julio.local>
2020-12-04 18:05:56 -0600

    added update tasks

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -25,9 +25,14 @@ def put_todo(user_id, desc):
     todos_collection_ref.add({'desc':desc, 'done':False})
 
 def delete_todo(user_id, todo_id):
-    todo_ref = db.document('users/{}/todos/{}'.format(user_id,todo_id))
+    todo_ref = _get_todo_ref(user_id, todo_id)
     todo_ref.delete()
 
     # todo_ref = db.collectio('users').document(user_id).collection('todos').document(todo_id)
 
-    
+def update_todo(user_id, todo_id, done):
+    todo_ref = _get_todo_ref(user_id, todo_id)
+    todo_ref.update({'done': not bool(done)})
+
+def _get_todo_ref(user_id, todo_id):
+    return db.document('users/{}/todos/{}'.format(user_id,todo_id))
